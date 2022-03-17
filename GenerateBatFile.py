0e42fa0fed28938d54de097bb561e5bf9086a2ad
Siwelthelongboi <42124064+SiwelTheLongBoi@users.noreply.github.com>
2022-03-17 18:18:42 +0000

    Update GenerateBatFile.py

--- GenerateBatFile.py
+++ GenerateBatFile.py
@@ -1,22 +1,24 @@
 StreamLink = "https://d1ymi26ma8va5x.cloudfront.net/47af4f688a4d3e34f513_ironmouse_44619847244_1644121175/chunked/index-dvr.m3u8"
-StreamSections = 94
+# Stream URL is https://www.twitch.tv/videos/1287350698
+StreamSections = 94 #How many 30 min sections are in the VOD
 
 '''
-Alternate values
+Another stream
+https://www.twitch.tv/videos/1288758764
 https://d1ymi26ma8va5x.cloudfront.net/ad7df46d01d076a6cab0_ironmouse_44606619692_1644025354/chunked/index-muted-FMTK8V9QQ2.m3u8
 53
 '''
 
 FileName = 'OutputFile'
 
-for i in range(0,30):#StreamSections + 1):
-    hour = 13#int(i / 2)
+for i in range(0,StreamSections + 1):
+    hour = int(i / 2)
     if hour >= 10:
         hour = str(hour)
     else:
         hour = '0' + str(hour)
     
-    minute = i#(i * 30) % 60
+    minute = (i * 30) % 60
     if minute == 0:
         minute = '00'
     else:
